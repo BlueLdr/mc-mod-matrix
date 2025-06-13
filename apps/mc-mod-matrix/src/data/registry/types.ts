431f60bdbcf23e6ace8414003d78e0e66dfd235e
egottlieb <egottlieb@mindgrub.com>
2025-06-13 10:41:08 -0400

    Switched from localStorage to IndexedDb

--- apps/mc-mod-matrix/src/data/registry/types.ts
+++ apps/mc-mod-matrix/src/data/registry/types.ts
@@ -1,3 +1,5 @@
+import type Dexie from "dexie";
+import type { EntityTable } from "dexie";
 import type * as Immutable from "immutable";
 import type { Mod } from "@mcmm/data";
 import type { Curseforge } from "@mcmm/curseforge";
@@ -8,9 +10,15 @@ export type DataRegistryMap = Immutable.Map<string | number, Mod>;
 export type DataRegistryEntryRecord = Record<string | number, DataRegistryEntry>;
 export type DataRegistryEntryMap = Immutable.Map<string | number, DataRegistryEntry>;
 
-export interface DataRegistryEntry {
+export interface DataRegistryDbEntryMeta {
   dateModified: number;
   minGameVersionFetched: string;
+}
+
+export interface DataRegistryDbEntry extends DataRegistryDbEntryMeta, Mod {}
+
+export interface DataRegistryEntry extends DataRegistryDbEntryMeta {
+  modId: Mod["id"];
   data: Mod;
 }
 
@@ -22,4 +30,9 @@ export interface DataRegistryStorageState {
   lastRefresh: number;
   registry: DataRegistryEntryRecord;
   // curseforgeVersionTypeRegistry: CurseforgeVersionTypeRegistry;
+}
+
+export interface DataRegistryDb extends Dexie {
+  lastRefresh: number;
+  registry: EntityTable<DataRegistryDbEntry, "id">;
 }
